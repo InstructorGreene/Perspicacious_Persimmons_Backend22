49c91b49fc7ccb6066cf8be9bc89ae1a25d6318a
Hanna Svietik <hsvietik@gmail.com>
2022-11-15 16:01:20 +0000

    user schema

--- models/booking.js
+++ models/booking.js
@@ -2,20 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 let bookingSchema = new Schema(
   {
-    name: {
-      type: String,
-      required: true,
-      lowercase: true,
-      trim: true,
-      minLength: [3, "Name is too short!"],
-      maxLength: 15,
-    },
     businessName: String,
-    email: {
-      type: String,
-      required: true,
-      lowercase: true,
-    },
     mobileNumber: {
       type: Number,
       required: true,
