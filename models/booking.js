05d71bc752d20efb5e40ebb234031512a567c1a9
Hanna Svietik <hsvietik@gmail.com>
2022-11-15 13:05:17 +0000

    booking schema

--- models/booking.js
+++ models/booking.js
@@ -1,15 +1,36 @@
 const mongoose = require("mongoose");
-
-const bookingSchema = mongoose.Schema({
-  name: String,
-  businessName: String,
-  email: String,
-  telephone: String,
-  type: String,
-  comments: String,
-  files: String,
-  status: String,
-  date: { type: Date, default: Date.now },
-});
+const Schema = mongoose.Schema;
+let bookingSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      minLength: [3, "Name is too short!"],
+      maxLength: 15,
+    },
+    businessName: String,
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    mobileNumber: {
+      type: Number,
+      required: true,
+      minLength: [11, "Telephone number is too short!"],
+      maxLength: [12, "Telephone number is too short!"],
+    },
+    stallType: String,
+    comments: String,
+    status: String,
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { collection: "bookings" }
+);
 
 module.exports.Booking = mongoose.model("Booking", bookingSchema);
