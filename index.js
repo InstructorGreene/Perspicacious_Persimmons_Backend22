837fee5bb93c6d736371e741b4c68b4a0ce4a8ed
Hanna Svietik <hsvietik@gmail.com>
2022-11-17 09:58:02 +0000

    code removed

--- index.js
+++ index.js
@@ -89,15 +89,6 @@ app.post("/", async (req, res) => {
   const newEvent = req.body;
   const event = new Booking(newEvent);
   await event.save();
-  try {
-    if (x == "") throw "empty";
-    if (isNaN(x)) throw "not a number";
-    x = Number(x);
-    if (x < 5) throw "too low";
-    if (x > 10) throw "too high";
-  } catch (err) {
-    message.innerHTML = "Input is " + err;
-  }
   res.send({ message: "New event inserted." });
 });
 
