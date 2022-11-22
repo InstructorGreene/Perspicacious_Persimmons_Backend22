1703e44efa9d43ca0522354e5e18b33aec900315
Hanna Svietik <hsvietik@gmail.com>
2022-11-22 15:32:59 +0000

    user schema changed

--- models/user.js
+++ models/user.js
@@ -5,18 +5,12 @@ const userSchema = mongoose.Schema(
     firstName: {
       type: String,
       required: true,
-      lowercase: true,
       trim: true,
-      minLength: [3, "Name is too short!"],
-      maxLength: 15,
     },
     lastName: {
       type: String,
       required: true,
-      lowercase: true,
       trim: true,
-      minLength: [3, "Name is too short!"],
-      maxLength: 15,
     },
     email: {
       type: String,
@@ -26,8 +20,6 @@ const userSchema = mongoose.Schema(
     mobileNumber: {
       type: Number,
       required: true,
-      minLength: [11, "Telephone number is too short!"],
-      maxLength: [12, "Telephone number is too short!"],
     },
     password: {
       type: String,
